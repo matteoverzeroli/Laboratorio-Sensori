b667810a9d431de4c11fee97a80bf0baab4e7a1e
fmartina4 <f.martina4@gmail.com>
2022-11-19 17:12:52 +0100

    added Matlab analysis and related plots

--- Laboratorio 15_11_22/OtherFiles/accelerometer.m
+++ Laboratorio 15_11_22/OtherFiles/accelerometer.m
@@ -12,9 +12,20 @@ fclose(s);
 
 %%
 t=0:1:length(arr2)-1;
-t=t*0.1;7
+t=t*0.1;
+% Grafico 1
+plot(t, arr2);
+legend('X', 'Y', 'Z');
+xlabel('t [s]');
+ylabel('a [mg]');
+
+% Grafico 2
 arr2 = arr2 - mean(arr2,1); 
 filt = movmean(arr2,10);
+plot(t, filt);
+legend('X', 'Y', 'Z');
+xlabel('t [s]');
+ylabel('a [mg]');
 
 freq=sum(diff(gradient(filt)>0)==-1,1)./10
 
@@ -27,10 +38,8 @@ P2 = abs(Y/L);
 P1 = P2(1:L/2+1);
 P1(2:end-1) = 2*P1(2:end-1);
 f = Fs*(0:(L/2))/L;
+% Grafico 3
 plot(f,P1) 
 title("Single-Sided Amplitude Spectrum of X(t)")
-xlabel("f (Hz)")
+xlabel("f [Hz]")
 ylabel("|P1(f)|")
-
-plot(t,filt)
-legend('X','Y','Z');
