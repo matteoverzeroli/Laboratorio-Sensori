5ef4120d74754fd955da59ea08a3f1b30ba36d79
Davide Salvetti <davide.salvetti97@live.com>
2022-11-25 12:59:20 +0100

    Small Fixes

--- Laboratorio 15_11_22/OtherFiles/metodo2.m
+++ Laboratorio 15_11_22/OtherFiles/metodo2.m
@@ -1,10 +1,10 @@
 %codice tratto da: https://it.mathworks.com/help/matlab/ref/fft.html
 
-%frequenza di campionamento
+%frequenza di campionamento della board
 Fs = 10;
 %periodo di campionamento
 T = 1/Fs;
-%vettore dei tempi
+%vettore dei tempi, filt E'il segnale filtrato
 tfft = (0:length(filt)-1)*T;
 %FFT della componente z del segnale filtrato
 Y = fft(filt(:,3));
@@ -13,7 +13,7 @@ Y = fft(filt(:,3));
 P2 = abs(Y/length(filt));
 P1 = P2(1:length(filt)/2+1);
 P1(2:end-1) = 2*P1(2:end-1);
-%calcolo dello spettro delle frequenze
+%calcolo dello spettro delle frequenze (asse x)
 f = Fs*(0:(length(filt)/2))/length(filt);
 %plot dello spettro in frequenza del segnale filtrato
 plot(f,P1) 
